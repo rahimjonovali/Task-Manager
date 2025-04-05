86fadcfb9f2a6034574849c7a310fed057619d27
Muhammadjon <su13445@studenti.polito.it>
2025-04-05 15:18:57 +0500

    some changes

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,10 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL')
+
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
